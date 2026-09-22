f46b44290ba29c43f3f970f8a39267981e359a55
BRUNO TAFFAREL ALVES DA SILVA <bruno.tasilva1@senacsp.edu.br>
2026-09-21 22:02:49 -0300

    adicionado rotas/tela de login/esqueleto outras telas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component'; 
+import { HomeComponent } from './pages/home/home.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
+import { ClientesComponent } from './pages/clientes/clientes.component';
+import { AjudaComponent } from './pages/ajuda/ajuda.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '',component: HomeComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'sobre', component: SobreComponent },
+    { path: 'clientes', component:  ClientesComponent},
+    { path: 'ajuda', component: AjudaComponent }
+];
